c7b801d3b6706b3ed31fbd51dff7007897b94121
parvat07 <bhusalparvat@gmail.com>
2025-10-25 23:38:54 +0100

    Finseer
    Added existing FinSeer to test environment

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
+import argparse
+from FinSeer.src.2_train_retriever.get_llm_feedback_scores import get_all_scores
 
-print("Hello, World!")
+parser = argparse.ArgumentParser(description='test')
+parser.add_argument('--dataset', default='acl18', type=str)
+parser.add_argument('--target', default='acl18.scored.json', type=str)
+args = parser.parse_args()
+get_all_scores(llm='StockLLM')  # llama family are all supported for this code
