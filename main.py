882e0290e68685ce3fbdf16ed8707f1356043440
parvat07 <bhusalparvat@gmail.com>
2025-11-10 10:29:58 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-"""
- import argparse
+
+import argparse
 from FinSeer.src.2_train_retriever.get_llm_feedback_scores import get_all_scores
 
 parser = argparse.ArgumentParser(description='test')
@@ -7,7 +7,3 @@ parser.add_argument('--dataset', default='acl18', type=str)
 parser.add_argument('--target', default='acl18.scored.json', type=str)
 args = parser.parse_args()
 get_all_scores(llm='StockLLM')  # llama family are all supported for this code 
-"""
-
-from datasets import load_dataset
-dataset = load_dataset("ElsaShaw/finseer_data")
